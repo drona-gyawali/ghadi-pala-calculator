08610ef90bee39729f6d358eb6faf01953f51c65
drona-gyawali <dronarajgyawali@gmail.com>
2025-07-31 23:35:28 +0545

    Updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,8 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Vedic Calculator',
+  description: 'This project is created by Dorna Raj Gyawali',
 }
 
 export default function RootLayout({
